e5d05177fdf723aea6218ce9f3252c525eeb52a0
alex1983ovdoc1 <bondar1983ovdoc1@gmail.com>
2023-11-22 16:52:30 +0200

    Add Deploy

---
+++ Deploy/fabfile.py
@@ -0,0 +1,46 @@
+# from fabric import Connection
+# from invoke import task
+
+# @task
+# def test(c):
+#     conn = Connection(host='34.82.228.195', user='bondar1983ovdoc1', connect_kwargs={"key_filename": "C:/.ssh/id_rsa"})
+#     conn.run('date')
+#     conn.local('date')
+
+
+
+
+from fabric.api import env, settings, run, local, cd, sudo
+
+env.hosts = ["34.82.228.195"]
+env.user = "bondar1983ovdoc1"
+# env.key_filename = "C:/.ssh/id_rsa"
+
+def test():
+    with settings(connect_kwargs={"key_filename": "C:/.ssh/id_rsa"}, warn_only=True):
+        run("date")
+        local("date")
+
+def update():
+	with settings(sudo_prompt="Password:", warn_only=True):
+		cd("/home/bondar1983ovdoc1/server_weather_news2023")
+		run("git pull")
+		sudo("supervisorctl restart flask")
+
+
+
+
+# from fabric.api import *
+
+# env.hosts = ["34.82.228.195"]
+# env.user = "bondar1983ovdoc1"
+# # env.reject_unknown_hosts = False
+# # env.disable_known_hosts = True
+
+# def test():
+# 	# with settings(connect_kwargs={"key": key}, warn_only=True):
+# 	# 	run("date")
+# 	# 	local("date")
+#     with settings(connect_kwargs={"key_filename": "C:/.ssh/id_rsa"}, warn_only=True):
+#         run("date")
+#         local("date")
