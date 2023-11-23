aa07442d1a7780eccb2e57d61fa4be6018421462
alex1983ovdoc1 <bondar1983ovdoc1@gmail.com>
2023-11-23 15:31:18 +0200

    comment 7889

--- webapp/utils.py
+++ webapp/utils.py
@@ -15,25 +15,56 @@ def is_safe_url(target):
 def get_redirect_target():
 
 	for target in request.values.get('next'), request.referrer:
+		print(type(target))
+		print(target)
 
 		referrer = request.referrer
 		parsed_url = urlparse(referrer)
-		# print(parsed_url)
+		print(parsed_url)
 
-		if 'None' in str(target) and target is not None or parsed_url.path=='/users/login':
-			target = f"{parsed_url.scheme}://{parsed_url.netloc}/{str(parsed_url.query).replace('next=', '')}"	
+		# if 'None' in str(target) and target is not None:
 
+		# 	print('1111')
+		# 	# target = f"{parsed_url.scheme}://{parsed_url.netloc}"	
+		# 	target = f"{parsed_url.scheme}://{parsed_url.netloc}/{str(parsed_url.query).replace('next=', '')}"	
+		# 	# break
 
+		# if target is None:
+		# 	target = f"{parsed_url.scheme}://{parsed_url.netloc}/{parsed_url.path}"	
+		# 	print('222')
+		# 	# break
+
+		# if  'next' in str(parsed_url.query):
+		# 	print('333')
+		# 	target = f"{parsed_url.scheme}://{parsed_url.netloc}/{str(parsed_url.query).replace('next=', '')}"
+
+		# if '/users/register' in target:   or parsed_url.path=='/users/login'
+		# 	target = f"{parsed_url.scheme}://{parsed_url.netloc}"
+		# 	print('444')	
+
+		if 'None' in str(target) and target is not None:
+			print('1111')
+			# target = f"{parsed_url.scheme}://{parsed_url.netloc}/{str(parsed_url.query).replace('next=', '')}"
+			if 'next' in str(parsed_url.query): 				
+				target = f"{parsed_url.scheme}://{parsed_url.netloc}/{str(parsed_url.query).replace('next=', '')}"
+			else:
+				target = f"{parsed_url.scheme}://{parsed_url.netloc}/{parsed_url.query}"
+		
 		if target is None:
-			target = f"{parsed_url.scheme}://{parsed_url.netloc}/{parsed_url.path}"	
+			print('222')
+			target = f"{parsed_url.scheme}://{parsed_url.netloc}/{parsed_url.path}"
 
-		if  'next' in str(parsed_url.query):
-			target = f"{parsed_url.scheme}://{parsed_url.netloc}/{str(parsed_url.query).replace('next=', '')}"
+		if 'login' in str(target) and 'news' not in str(target):
+			print('333')
+			target = f"{parsed_url.scheme}://{parsed_url.netloc}"
+		
+		if 'login' in str(target) and 'news' in str(target):
+			print('444')
+			if 'next' in str(parsed_url.query): 				
+				target = f"{parsed_url.scheme}://{parsed_url.netloc}/{str(parsed_url.query).replace('next=', '')}"
+			else:
+				target = f"{parsed_url.scheme}://{parsed_url.netloc}/{parsed_url.query}"
 
-		if '/users/register' in target:
-			target = f"{parsed_url.scheme}://{parsed_url.netloc}"	
-
-		# print(target)
 
 		if not target:
 			continue
@@ -41,10 +72,8 @@ def get_redirect_target():
 			list_url = ['/users/login', '/users/login?next=/users/register?', '/admin', '/admin/?next=/?', '/?', 'None', '/users/None']
 
 			if target in list_url:
-				# target = f'http://127.0.0.1:5000/'	
-				# referrer = request.referrer
-				# parsed_url = urlparse(referrer)
 				target = f"{parsed_url.scheme}://{parsed_url.netloc}"
 
+		print(target)
 
 	return target
