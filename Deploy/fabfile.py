dbd69877940c3a3c8eb0345cffc97c90efb329d4
alex1983ovdoc1 <bondar1983ovdoc1@gmail.com>
2023-11-24 19:06:52 +0200

    comment 4592

--- Deploy/fabfile.py
+++ Deploy/fabfile.py
@@ -32,26 +32,3 @@ def update():
 		run("git stash")
 		run("git pull")
 		sudo("supervisorctl restart flask")
-
-	# with settings(sudo_prompt="Password:", warn_only=True):
-	# 	cd("/home/bondar1983ovdoc1/server_weather_news2023")
-	# 	run("git pull")
-	# 	sudo("supervisorctl restart flask")
-
-
-
-
-# from fabric.api import *
-
-# env.hosts = ["34.82.228.195"]
-# env.user = "bondar1983ovdoc1"
-# # env.reject_unknown_hosts = False
-# # env.disable_known_hosts = True
-
-# def test():
-# 	# with settings(connect_kwargs={"key": key}, warn_only=True):
-# 	# 	run("date")
-# 	# 	local("date")
-#     with settings(connect_kwargs={"key_filename": "C:/.ssh/id_rsa"}, warn_only=True):
-#         run("date")
-#         local("date")
